45fe0bf209d166d001a8415f571cb0d564ced18f
reaperhound <reaperhound69004@gmail.com>
2023-04-10 13:48:29 +0530

    GPT code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "../App.css";
 
 const Hero = () => {
-  const [quote, setQuote] = useState("ibijk");
+  const [quote, setQuote] = useState(null);
 
   useEffect(() => {
     fetch("https://animechan.vercel.app/api/random")
@@ -26,37 +26,28 @@ const Hero = () => {
           <div className="flex items-center justify-start sm:min-w-[50vw] sm:min-h-[20vh] p-5 ">
             <div className="z-10 sm:mx-[200px] ">
               <p className="font-semibold text-lg ">
-                {quote.quote}{console.log(quote)}
-                {/* Quote Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Quidem repellendus expedita autem omnis esse delectus enim ea
-                hic! */}
+                "{quote.quote}"
               </p>
               <div className="flex justify-between mt-1">
                 <span className="anime-font">
                   {quote.anime}
-                  {/* animeName */}
                 </span>
                 <span className="anime-font">
                   {quote.character}
-                  {/* animeCharacter */}
                 </span>
               </div>
-            </div>
-
-            <div className="sm:ml-[900px] mb-[500px] absolute z-0">
             </div>
           </div>
         </>
       ) : (
         <div className="sm:min-w-[50vw] sm:min-h-[20vh] min-w-[50vw] min-h-[20vh] sm:mx-[50vw] relative mx-[20vw] ">
           {/* //Progress Bar */}
-          <img src="./loading-bar.svg" alt="" srcset="" />
+          <img src="./loading-bar.svg" alt="Loading..." />
         </div>
       )}
-      {/* <button onClick={handleButtonClick}>New Quote</button> */}
       <button 
       onClick={handleButtonClick}
-      className="sm:ml-[220px] sm:mt-5 ml-5 mt-5 btn btn-outline btn-primary">
+      className="mt-5 ml-[220px] btn btn-outline btn-primary">
         New Quote
       </button>
     </div>
